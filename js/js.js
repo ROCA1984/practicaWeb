83d3a1dc7cb10aba4ea04957df979aca321ee730
rocalinux <rodrigo.m.carrasco.c@gmail.com>
2022-05-20 22:27:36 -0400

    Practica 01 Bootstrap 5 Terminada

---
+++ js/js.js
@@ -0,0 +1,11 @@
+ /*Inicializar Popovers*/
+ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+ var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+   return new bootstrap.Popover(popoverTriggerEl)
+ })
+
+ /*Inicializar Tootips*/
+ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+   return new bootstrap.Tooltip(tooltipTriggerEl)
+ })
